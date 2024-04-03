e0ff283419db4e802cb4fc4d3eb7a2fb986b3493
sherin17 <sherinthomasvc@gmail.com>
2024-04-03 11:11:23 +0100

    mongodb

--- middlewares/mongodb.js
+++ middlewares/mongodb.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose')
+// Retrieve connection details from environment variables
+const DB_URL = process.env.DATABASE_URL;
+const DB_NAME = process.env.DATABASE_NAME;
+
+// Connect to MongoDB using mongoose
 mongoose.connect('mongodb+srv://Sherin17:Reenijiji1@cluster0.quvjjgb.mongodb.net/JobLinkers?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-. then(()=>{
-    console.log('mongoDB connected successfully!!!')
-}).catch((error)=>{
-    console.log(error.message)
-})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
